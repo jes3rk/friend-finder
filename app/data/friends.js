999a21f8182a542d49253a2985093929103d9189
Joseph Schefer <joseph.schefer@gmail.com>
2017-11-19 10:44:30 -0500

    The POST function works. Working on the return now

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@ var friends = [
   {
     "name": "Ahmed",
     "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores[]": [
+    "scores": [
         5,
         1,
         4,
@@ -14,6 +14,22 @@ var friends = [
         4,
         1
       ]
+  },
+  {
+    "name": "Brad",
+    "photo": "https://images-na.ssl-images-amazon.com/images/M/MV5BMjA1MjE2MTQ2MV5BMl5BanBnXkFtZTcwMjE5MDY0Nw@@._V1_UY1200_CR84,0,630,1200_AL_.jpg",
+    "scores": [
+      3,
+      5,
+      1,
+      4,
+      5,
+      5,
+      2,
+      1,
+      3,
+      2
+    ]
   }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,19 +11,21 @@ module.exports = function(app) {
   app.post("/api/friends", function(req, res) {
 
     var survey = req.body;
-    var surveyScores = req.body["scores[]"];
+    // output = friendData;
     var comp = [];
 
     for (var i = 0; i < friendData.length; i++) {
-      for (var j = 0; j <= 10; j++) {
-        var diff = Math.abs(surveyScores[j] - friendData[i].scores[j]);
-        comp.push(diff);
+      var diff = 0;
+      for (var j = 0; j < friendData[i].scores.length; j++) {
+        // Diff is the sum of the absolute values of the differences in each score
+        diff += Math.abs(survey.scores[j] - friendData[i].scores[j]);
       };
+      comp.push(diff);
     };
-    output = survey;
-
-    var minIndex = comp.indexOf(Math.min(comp));
-    output = friendData[minIndex];
+    var min = Math.min(comp);
+    var indexMin = comp.indexOf(min);
+    output = friendData[indexMin];
     friendData.push(survey);
+    // output = friendData;
   });
 };
